d9228ed615523da1c6002b3f4e6aaf1d0babf8c2
MaksFenek <makstreonin@gmail.com>
2021-04-01 14:44:29 +0300

    Added welcome page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    semi: ['warn', 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
---
+++ src/components/pages/WelcomePage.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {Image, SafeAreaView, StyleSheet, View, Text} from 'react-native'
+import {Button} from 'react-native-elements'
+
+const WelcomePage: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.bg}>
+      <Image style={styles.bgImage} source={require('../../assets/bg.png')} />
+      <View style={styles.overlay} />
+      <View style={styles.imgContainer}>
+        <Image style={styles.img} source={require('../../assets/logo.png')} />
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.text}>
+          The Little Coffee Shop serves specialty {'\n'} coffee, fancy grilled
+          sandwiches, {'\n'} scratch cooking, craft ales, and cider.
+        </Text>
+        <Button buttonStyle={styles.button} title="Sign in" />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default WelcomePage
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    position: 'relative',
+    padding: 20,
+  },
+  bgImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(48,49,50,0.8)',
+    zIndex: 1,
+  },
+  imgContainer: {
+    flex: 0.7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  descriptionContainer: {
+    flex: 0.4,
+    justifyContent: 'space-around',
+  },
+  img: {
+    height: 150,
+    width: 150,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    lineHeight: 38,
+    color: '#fff',
+    fontWeight: '300',
+  },
+  button: {
+    backgroundColor: '#0D9F67',
+    color: 'black',
+    borderRadius: 0,
+    height: 60,
+  },
+})
